8c71d7bab0e3b8851bdb8594461275d5229d7230
AirealConduit <Ariel18555@gmail.com>
2023-08-11 11:48:00 -0400

    Made Changes to overall design / Tested

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import topImage from './images/PowerWash.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={topImage} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.............
+          Welcome to Eshkol's Cleaners
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to schedule
         </a>
       </header>
+      <body className="App-body">
+      <p>
+          This is the BODY
+        </p>
+      </body>
+      <footer className="App-footer">
+        <p>
+          This is the FOOTER
+        </p>
+      </footer>
     </div>
   );
 }
