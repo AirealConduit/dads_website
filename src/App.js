707becc916f3cade5893dddb28026553d09c5e22
AirealConduit <Ariel18555@gmail.com>
2023-08-11 16:24:06 -0400

    Added top nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+              <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
       <header className="App-header">
         <img src={topImage} className="App-logo" alt="logo" />
         <p>
